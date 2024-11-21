a1338b1819f1266b1c4a89a3be322a5947d904b5
vuniem131104 <niemhoalongvu@gmail.com>
2024-11-21 17:45:27 +0700

    Backup

--- src/FE/src/components/MainContent/MainContent.js
+++ src/FE/src/components/MainContent/MainContent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import { useEffect, useSta } from 'react';
 import './MainContent.css';
 
 function MainContent() {
--- src/server/index.js
+++ src/server/index.js
@@ -14,6 +14,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
