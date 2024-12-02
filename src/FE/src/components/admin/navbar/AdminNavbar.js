1bae9a2a287f9cae368efc16175ef46944326e2d
hotcat190 <nguyendinhnguyen151204@gmail.com>
2024-12-02 15:53:23 +0700

    remove admin sidebar-header

--- src/FE/src/components/admin/navbar/AdminNavbar.js
+++ src/FE/src/components/admin/navbar/AdminNavbar.js
@@ -4,9 +4,6 @@ import './AdminNavbar.css';
 export default function AdminNavbar() {
     return (
         <nav className="admin-sidebar">
-            <div className="sidebar-header">
-                <h2>Admin Panel</h2>
-            </div>
             <ul className="sidebar-menu">
                 <li>
                     <NavLink to="/admin/dashboard" className={({ isActive }) => isActive ? 'active' : ''}>
