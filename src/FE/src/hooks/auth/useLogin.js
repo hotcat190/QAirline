07bd225794c42b284062748f3af64dec4e98c6c4
hotcat190 <nguyendinhnguyen151204@gmail.com>
2024-12-07 16:50:25 +0700

    feat: email login signup

--- src/FE/src/hooks/auth/useLogin.js
+++ src/FE/src/hooks/auth/useLogin.js
@@ -3,48 +3,52 @@ import { useInputValidation } from "hooks/input/useInputValidation";
 import { BACKEND_BASE_URL } from "services/api";
 import isEmail from "validator/es/lib/isEmail";
 import isVietnameseMobilePhone from "utils/isVietnameseMobilePhone";
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from 'contexts/AuthContext';
 
 export const useLogin = () => {
-  const { value, handleChange: setEmail} = useInputValidation(
+  const { value: email, handleChange: setEmail} = useInputValidation(
     "Please enter a valid email address or phone number.",
     isEmail, isVietnameseMobilePhone);
   const [password, setPassword] = useState("");  
 
+  const navigate = useNavigate();
+  const { login } = useAuth();
+
   const onLoginSubmit = async (e) => {
-    e.preventDefault(); // Prevent default form submission
-
-    const jsonData = (isEmail(value)) ? { 
-      email: value,
-      password, 
-    } : {
-      numberPhone: value,
-      password,
-    }; // Prepare JSON payload
-
+    e.preventDefault();
     try {
+      // Make your API call to authenticate
       const response = await fetch(`${BACKEND_BASE_URL}/auth/login`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(jsonData),
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
       });
 
-      if (response.ok) {
-        const result = await response.json();
-        console.log("Login successful:", result.role);
-        // Handle success
-      } else {
-        console.error("Login failed:", (await response.text()).toString());
-        // Handle error
+      if (!response.ok) {
+        throw new Error('Login failed');
       }
+
+      const userData = await response.json();
+      const user = {role: userData.role, idCustomer: userData.idCustomer};
+      // Use the login function from AuthContext to save user data
+      login(user);
+      if (user.role === 'admin') {
+        navigate('/admin');
+      }
+      return { success: true };     
+
     } catch (error) {
-      console.error("An error occurred:", error);
-      // Handle network errors
+      console.error('Login error:', error);
+      // Handle error (show error message to user)
     }
   };
 
   return {
     onLoginSubmit,
-    value,
+    email,
     setEmail,
     password,
     setPassword,
