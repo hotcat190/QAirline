b488319d35a6328bf55c185373ab1765d6eae8d4
Tran Duc Luong <luong8076@gmail.com>
2024-12-21 15:08:13 +0700

    feat: update ticketDetail, update api ticket

--- src/FE/src/components/Header/Header.js
+++ src/FE/src/components/Header/Header.js
@@ -1,26 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from 'contexts/AuthContext';
-import { FaBell } from 'react-icons/fa';
-import './Header.css';
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "contexts/AuthContext";
+import { FaBell } from "react-icons/fa";
+import { BACKEND_BASE_URL } from "services/api";
+import "./Header.css";
 
 const Notification = () => {
-  const [ notifications, setNotifications ] = useState([]);
-  const [ unreadCount, setUnreadCount ] = useState(0);
-  const [ isDropdownOpen, setIsDropdownOpen ] = useState(false);
-  const [ error, setError ] = useState(null);
+  const [notifications, setNotifications] = useState([]);
+  const [unreadCount, setUnreadCount] = useState(0);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchNotifications = async () => {
-      const url = 'https://qairline.onrender.com/api/user/getNotification';
+      const url = `${BACKEND_BASE_URL}/user/getNotification`;
 
       try {
         const response = await fetch(url, {
-          method: 'GET',
+          method: "GET",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
-          credentials: 'include',
+          credentials: "include",
         });
 
         if (!response.ok) {
@@ -31,13 +32,15 @@ const Notification = () => {
 
         if (Array.isArray(data)) {
           setNotifications(data);
-          const unread = data.filter((notification) => notification.unRead).length;
+          const unread = data.filter(
+            (notification) => notification.unRead
+          ).length;
           setUnreadCount(unread);
         } else {
-          throw new Error('Unexpected response format');
+          throw new Error("Unexpected response format");
         }
       } catch (err) {
-        console.error('Error fetching notifications:', err);
+        console.error("Error fetching notifications:", err);
         setError(err.message);
       }
     };
@@ -45,61 +48,58 @@ const Notification = () => {
     fetchNotifications();
   }, []);
 
-
   return (
     <div className="notification-container">
-      <div className="notification-icon" onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
-        <div
-          className="chuong"
-        >
+      <div
+        className="notification-icon"
+        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+      >
+        <div className="chuong">
           <FaBell className="icon-chuong" />
           {unreadCount > 0 && (
-            <span
-              className="notification-count"
-
-            >
-              {unreadCount}
-            </span>
+            <span className="notification-count">{unreadCount}</span>
           )}
         </div>
       </div>
 
-      {isDropdownOpen && (<div className="notification-dropdown">
-        {error ? (
-          <p className="error-message" style={{ color: 'red' }}>
-            Error: {error}
-          </p>
-        ) : notifications.length > 0 ? (
-          notifications.map((notification, index) => (
-            <div
-              key={index}
-              className={`notification-item ${notification.unRead ? 'unread' : ''}`}
-            >
-              <p>{notification.content}</p>
-              <span className="notification-time">
-                {new Date(notification.create_at).toLocaleString()}
-              </span>
-            </div>
-          ))
-        ) : (
-          <p className="no-notifications">No notifications</p>
-        )}
-      </div>
-      )
-      }
+      {isDropdownOpen && (
+        <div className="notification-dropdown">
+          {error ? (
+            <p className="error-message" style={{ color: "red" }}>
+              Error: {error}
+            </p>
+          ) : notifications.length > 0 ? (
+            notifications.map((notification, index) => (
+              <div
+                key={index}
+                className={`notification-item ${
+                  notification.unRead ? "unread" : ""
+                }`}
+              >
+                <p>{notification.content}</p>
+                <span className="notification-time">
+                  {new Date(notification.create_at).toLocaleString()}
+                </span>
+              </div>
+            ))
+          ) : (
+            <p className="no-notifications">No notifications</p>
+          )}
+        </div>
+      )}
     </div>
   );
 };
 
 const UserDropdown = ({ avatar, setAvatar }) => {
   const { user, logout } = useAuth();
-  const [ showDropdown, setShowDropdown ] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
   const navigate = useNavigate();
 
   const handleLogout = () => {
     setShowDropdown(false);
     if (logout()) {
-      navigate('/');
+      navigate("/");
       window.location.reload();
     }
   };
@@ -108,7 +108,7 @@ const UserDropdown = ({ avatar, setAvatar }) => {
     <div className="dropdown-container">
       {/* Avatar */}
       <img
-        src={avatar || "img/default_avatar.png"}  // Cập nhật avatar khi có thay đổi
+        src={avatar || "img/default_avatar.png"} // Cập nhật avatar khi có thay đổi
         alt="User Avatar"
         className="user-avatar"
         onClick={() => setShowDropdown(!showDropdown)}
@@ -119,10 +119,18 @@ const UserDropdown = ({ avatar, setAvatar }) => {
         <div className="dropdown-menu">
           {/* Header Info */}
           <div className="dropdown-header">
-            <img src={avatar || "img/default_avatar.png"} alt="Avatar" className="dropdown-avatar" />
+            <img
+              src={avatar || "img/default_avatar.png"}
+              alt="Avatar"
+              className="dropdown-avatar"
+            />
             <div className="dropdown-user-info">
-              <span className="dropdown-name">{user.name || 'Hoàng Vũ Lê'}</span>
-              <span className="dropdown-username">@{user.username || 'lehoangvu6'}</span>
+              <span className="dropdown-name">
+                {user.name || "Hoàng Vũ Lê"}
+              </span>
+              <span className="dropdown-username">
+                @{user.username || "lehoangvu6"}
+              </span>
             </div>
           </div>
 
@@ -160,22 +168,22 @@ const UserDropdown = ({ avatar, setAvatar }) => {
 
 const Header = () => {
   const { user } = useAuth();
-  const [ avatar, setAvatar ] = useState('img/default_avatar.png');  // Thêm state avatar
+  const [avatar, setAvatar] = useState("img/default_avatar.png"); // Thêm state avatar
 
   const openSignin = () => {
-    document.querySelector('.overlay').style.display = 'block';
-    const signinForm = document.querySelector('.signin-form');
-    signinForm.style.display = 'flex';
-    setTimeout(() => signinForm.classList.add('showSign'), 10);
-    document.body.classList.add('no-scroll');
+    document.querySelector(".overlay").style.display = "block";
+    const signinForm = document.querySelector(".signin-form");
+    signinForm.style.display = "flex";
+    setTimeout(() => signinForm.classList.add("showSign"), 10);
+    document.body.classList.add("no-scroll");
   };
 
   const openSignup = () => {
-    document.querySelector('.overlay').style.display = 'block';
-    const signupForm = document.querySelector('.signup-form');
-    signupForm.style.display = 'flex';
-    setTimeout(() => signupForm.classList.add('showSign'), 10);
-    document.body.classList.add('no-scroll');
+    document.querySelector(".overlay").style.display = "block";
+    const signupForm = document.querySelector(".signup-form");
+    signupForm.style.display = "flex";
+    setTimeout(() => signupForm.classList.add("showSign"), 10);
+    document.body.classList.add("no-scroll");
   };
 
   return (
@@ -184,9 +192,7 @@ const Header = () => {
         <nav className="navbar">
           <label htmlFor="menu-checkbox" className="toggle-menu">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-              <path
-                d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"
-              />
+              <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
             </svg>
           </label>
           {/* Logo */}
